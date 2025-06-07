04643d2cef1ea52cd7ddfaf7024c436d08987e15
Sean Courtney <seancourtney@fab4it.com>
2025-06-06 21:25:11 -0500

    [LTD-002] Removed stray error log in RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        error_log("got here");
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
