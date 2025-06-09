4d4f8898230975d51bd8783ef1e96afcd8cfc7d5
Sean Courtney <seancourtney@fab4it.com>
2025-06-08 20:47:34 -0500

    [LTD-005] Fix weird merge conflict thing with routes

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,8 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login'])->name('login');
 Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
-<<<<<<< Updated upstream
-=======
 Route::get('/profile', [App\Http\Controllers\UserController::class, 'profile'])->name('profile');
 Route::put('/update/{user}/', [App\Http\Controllers\UserController::class, 'update'])->name('user.update');
->>>>>>> Stashed changes
 
 Route::get('/privacy', function() {
     return view('privacy');
