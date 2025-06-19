c1e9ae8f4030610ed28541ec14fb8f829b25bd48
Sean Courtney <seancourtney@fab4it.com>
2025-06-19 17:38:43 -0500

    [LTD-007] Dynamically generate wide tiles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,55 @@ class HomeController extends Controller
      */
     public function index(): View
     {
-        return view('index');
+        if (Auth::check()) {
+            $tiles = array(
+                array(
+                    'activity' => 'meal',
+                    'icon'     => 'feed',
+                ),
+                array(
+                    'activity' => 'walk',
+                    'icon'     => 'dogwalk',
+                    'class'    => 'walkBox',
+                ),
+                array(
+                    'activity' => 'potty',
+                    'icon'     => 'potty',
+                    'class'    => 'pottyBox',
+                ),
+                array(
+                    'activity' => 'treat',
+                    'icon'     => 'treat',
+                    'class'    => 'treatBox',
+                ),
+                array(
+                    'activity' => 'med',
+                    'icon'     => 'meds',
+                    'class'    => 'medBox',
+                ),
+                array(
+                    'activity' => 'bath',
+                    'icon'     => 'bath',
+                    'class'    => 'bathBox',
+                ),
+                array(
+                    'activity' => 'dog_registry',
+                    'icon'     => 'registry',
+                    'class'    => 'registerBox',
+                    'nolog'    => true,
+                ),
+                array(
+                    'activity' => 'fast_glance',
+                    'icon'     => 'kwik',
+                    'class'    => 'quickBox',
+                    'nolog'    => true,
+                )
+            );
+
+            $return_value = view('index', compact('tiles'));
+        } else {
+            $return_value = view('index');
+        }
+        return $return_value;
     }
 }
--- resources/views/components/tile.blade.php
+++ resources/views/components/tile.blade.php
@@ -1,6 +1,6 @@
 <div class="menuBox container py-2 {{ $class ?? '' }}">
     <a class="menuAnchor no-dec" href="
-        @if(isset($nolog))
+        @if(isset($nolog) && $nolog)
             {{ route(Str::beforeLast($activity, '_') . '.' . Str::afterLast($activity, '_')) }}
         @else
             {{ route('logit.' . $activity) }}
@@ -8,11 +8,11 @@
         ">
         <img src={{ asset("images/menu_$icon.png") }} alt="Log a {{ $activity }}" class="img-fluid" />
         <div class="menuText"><span class="d-none d-md-inline">
-            @if(!isset($nolog))
-                Log a </span>{{ Str::ucfirst($activity) }}</div>
-            @else
+            @if(isset($nolog) && $nolog)
                 {{ Str::of($activity)->beforeLast('_')->ucfirst() }}</span>
                 {{ Str::of($activity)->afterLast('_')->ucfirst() }}</div>
+            @else
+                Log a </span>{{ Str::ucfirst($activity) }}</div>
             @endif
     </a>
 </div>
